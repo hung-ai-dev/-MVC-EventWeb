ad4d939a308caf527384af52b0dd69bb4c84d555
Hưng Nguyễn <ndhung.uet@gmail.com>
2016-06-18 16:35:04 +0700

    first commit

---
+++ EventWeb/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventWeb.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+
+        [Required, MaxLength(255)]
+        public string Name { get; set; }
+    }
+}
---
+++ EventWeb/Models/Gig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventWeb.Models
+{
+    public class Gig
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required, MaxLength(255)]
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+    }
+}
