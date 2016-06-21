99408f160f9d084d7db0f603cf520e47010851cb
Hưng Nguyễn <Hng Nguyn>
2016-06-21 16:34:56 +0700

    ....

---
+++ EventWeb/Controllers/GigsController.cs
@@ -0,0 +1,28 @@
+﻿using EventWeb.Models;
+using EventWeb.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventWeb.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            GigFormViewModel viewModel = new GigFormViewModel();
+            viewModel.Genres = _context.Genres.ToList();
+            return View(viewModel);
+        }
+    }
+}
