d6cbf48ddc13e3cb74d0a8ea1d211d2359742f07
coten <coten@DESKTOP-7OA9E0Q>
2016-07-05 21:45:24 +0700

    Update - Done

--- EventWeb/Controllers/GigsController.cs
+++ EventWeb/Controllers/GigsController.cs
@@ -3,9 +3,9 @@ using System.Data.Entity;
 using EventWeb.Models;
 using EventWeb.ViewModels;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
+using System.Globalization;
 
 namespace EventWeb.Controllers
 {
@@ -35,6 +35,16 @@ namespace EventWeb.Controllers
         }
 
         [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs =
+                _context.Gigs.Where(u => u.ArtistId == userId && u.DateTime > DateTime.Now).Include(g => g.Artist)
+                    .Include(g => g.Genre).ToList();
+            return View(gigs);
+        }
+
+        [Authorize]
         public ActionResult Following()
         {
             var userId = User.Identity.GetUserId();
@@ -52,9 +62,13 @@ namespace EventWeb.Controllers
         [Authorize]
         public ActionResult Create()
         {
-            GigFormViewModel viewModel = new GigFormViewModel();
-            viewModel.Genres = _context.Genres.ToList();
-            return View(viewModel);
+            GigFormViewModel viewModel = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList(),
+                Heading = "Add a Event"
+            };
+           
+            return View("GigForm", viewModel);
         }
 
         [Authorize]
@@ -64,9 +78,9 @@ namespace EventWeb.Controllers
             if (!ModelState.IsValid)
             {
                 viewModel.Genres = _context.Genres.ToList();
-                return View("Create", viewModel);
+                return View("GigForm", viewModel);
             }
-                
+
             var gig = new Gig()
             {
                 ArtistId = User.Identity.GetUserId(),
@@ -77,7 +91,47 @@ namespace EventWeb.Controllers
 
             _context.Gigs.Add(gig);
             _context.SaveChanges();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.ArtistId == userId && g.Id == id);
+
+            var viewModel = new GigFormViewModel()
+            {
+                Id = gig.Id,
+                Genres = _context.Genres.ToList(),
+                Venue = gig.Venue,
+                Date = gig.DateTime.ToString("d/MMM/yyyy", CultureInfo.CreateSpecificCulture("en-us")),
+                Time = gig.DateTime.ToString("HH:mm"),
+                Genre = gig.GenreId,
+                Heading = "Edit your event"
+            };
+            return View("GigForm", viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Update(GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("GigForm", viewModel);
+            }
+
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == viewModel.Id && g.ArtistId == userId);
+
+            gig.Venue = viewModel.Venue;
+            gig.DateTime = viewModel.GetDateTime();
+            gig.GenreId = viewModel.Genre;
+
+            _context.SaveChanges();
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }
--- EventWeb/ViewModels/GigFormViewModel.cs
+++ EventWeb/ViewModels/GigFormViewModel.cs
@@ -2,13 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace EventWeb.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
+        public int Id { get; set; }
+
         [Required]
         public string Venue { get; set; }
         
@@ -23,6 +24,14 @@ namespace EventWeb.ViewModels
         public byte Genre { get; set; }
 
         public IEnumerable<Genre> Genres { get; set; }
+
+        public string Heading { get; set; }
+
+        public string Action
+        {
+            get { return (Id != 0) ? "Update" : "Create"; }
+        }
+
         public DateTime GetDateTime() {
             return DateTime.Parse($"{Date} {Time}");
         }
